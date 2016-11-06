dc77efba13f3a2c2bfa8087ef3d9a355386c347a
nathaliesicard <nathalie.sicard@gmail.com>
2016-11-06 17:15:14 -0600

    Added koa-nunjucks-render and koa-better-static

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
-var koa = require('koa');
-var app = koa();
+const koa = require('koa');
+const app = koa();
+const serve = require('koa-better-static');
+const nunjucks = require('koa-nunjucks-render');
+const Router = require ('koa-router');
+
 
 // logger
 
@@ -10,12 +14,38 @@ app.use(function *(next){
   console.log('%s %s - %s', this.method, this.url, ms);
 });
 
-// response
+app.use(serve('public'));
 
-app.use(function *(){
-  this.body = 'Hello World';
+app.use(nunjucks('views', {
+  ext: '.html',
+  noCache: true
+}));
+
+
+var router = new Router();
+
+var paths = {
+  '/': 'index'
+};
+
+Object.keys(paths).forEach(function(path) {
+  router.get(path, function*() {
+    yield this.render(paths[path]);
+  });
 });
+
+app.use(router.routes());
+
+
+app.use(function*() {
+
+  this.status = 404;
+  this.body = 'Page not found';
+
+});
+
 
 let port = process.env.PORT || 3000;
 
 app.listen(port);
+console.log('Listening to port ',port);
